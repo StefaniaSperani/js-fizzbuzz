13e5a32abd07e4493b5873fa6436a44c64efa8d4
StefaniaSperani <thedarkcry@hotmail.it>
2022-10-10 16:41:40 +0200

    creato ciclo

--- js/script.js
+++ js/script.js
@@ -4,3 +4,29 @@
 // ma che per i multipli di 3 stampi “Fizz” al posto del numero 
 // e per i multipli di 5 stampi “Buzz”. 
 // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+
+// creo un ciclo che mi dia dei numeri da 1 a 100
+for(let i = 1; i <= 100; i++){
+    // console.log(i);
+
+    // se i numeri sono divisibili sia per 3 che per 5
+    if((i % 3 == 0) && (i % 5 == 0)){
+        console.log('fizzbuzz');
+
+    // se i numeri sono divisibili per 5
+    } else if(i % 5 == 0){
+        console.log('buzz');
+
+     // se i numeri sono divisibili per 3
+    } else if(i % 3 == 0){
+        console.log('fizz');
+    // se non sono divisibili ne per 3 e nemmeno per 5
+    } else{
+        console.log(i + 'else');
+    }
+
+
+    // const square = document.createElement('div');
+
+};
