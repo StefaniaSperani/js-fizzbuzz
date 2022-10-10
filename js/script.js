2e304b60e4de0cb8a9a33b3857ca3ed4666332f4
StefaniaSperani <thedarkcry@hotmail.it>
2022-10-10 18:36:20 +0200

    bonus 1 + 2

--- js/script.js
+++ js/script.js
@@ -5,31 +5,42 @@
 // e per i multipli di 5 stampi “Buzz”. 
 // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
+const wrapper = document.querySelector('.wrapper');
+// console.log(wrapper);
 
 // creo un ciclo che mi dia dei numeri da 1 a 100
 for(let i = 1; i <= 100; i++){
     // console.log(i);
 
-    const wrapper = document.getElementsByClassName('wrapper');
+    // creo un nuovo div che sarà il mio quadrato
     const myDiv = document.createElement('div');
     myDiv.className = 'square';
-    console.log(myDiv);
+    // console.log(myDiv);
 
     // se i numeri sono divisibili sia per 3 che per 5
     if((i % 3 == 0) && (i % 5 == 0)){
-        // console.log('fizzbuzz');
+        myDiv.textContent='FizzBuzz';
+        myDiv.classList = 'fizzbuzz';
+        // console.log(myDiv);
 
     // se i numeri sono divisibili per 5
     } else if(i % 5 == 0){
-        // console.log('buzz');
+        myDiv.textContent='Buzz';
+        myDiv.classList = 'buzz';
+        // console.log(myDiv);
 
      // se i numeri sono divisibili per 3
     } else if(i % 3 == 0){
-        // console.log('fizz');
+        myDiv.textContent='Fizz';
+        myDiv.classList = 'fizz';
+        // console.log(myDiv);
 
     // se non sono divisibili ne per 3 e nemmeno per 5
     } else{
+        myDiv.append(i);
         // console.log(i + 'else');
     }
+
+    wrapper.append(myDiv);
 
 };
