620658626fdc472b2145d02be500e5b428e27f70
StefaniaSperani <thedarkcry@hotmail.it>
2022-10-10 17:02:43 +0200

    creating wrapper and div

--- js/script.js
+++ js/script.js
@@ -10,23 +10,27 @@
 for(let i = 1; i <= 100; i++){
     // console.log(i);
 
+    const wrapper = document.getElementsByClassName('wrapper');
+    const myDiv = document.createElement('div');
+    myDiv.className = 'square';
+    // console.log(myDiv);
+
     // se i numeri sono divisibili sia per 3 che per 5
     if((i % 3 == 0) && (i % 5 == 0)){
-        console.log('fizzbuzz');
+        // console.log('fizzbuzz');
 
     // se i numeri sono divisibili per 5
     } else if(i % 5 == 0){
-        console.log('buzz');
+        // console.log('buzz');
 
      // se i numeri sono divisibili per 3
     } else if(i % 3 == 0){
-        console.log('fizz');
+        // console.log('fizz');
+
     // se non sono divisibili ne per 3 e nemmeno per 5
     } else{
         console.log(i + 'else');
     }
 
-
-    // const square = document.createElement('div');
-
+    wrapper.append(myDiv);
 };
